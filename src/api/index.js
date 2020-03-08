6ed62bdf5b7cbe21f6677a7aa0193443acb204a3
jprittie <jennifer.prittie@gmail.com>
2020-03-07 19:51:53 -0500

    Install swagger-ui-express and create swagger.json

--- src/api/index.js
+++ src/api/index.js
@@ -4,6 +4,12 @@ const { healthRouter } = require('../routes/health/health.router')
 const { postsRouter } = require('../routes/posts/posts.router')
 
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+router.use('/api-docs', swaggerUi.serve);
+router.get('/api-docs', swaggerUi.setup(swaggerDocument));
+
 router.use("/health", healthRouter);
 router.use("/posts", postsRouter);
 
